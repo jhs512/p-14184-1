026ef3e3e7b8c62ada7c65421af4916bd3d15fec
jhs512 <jangka512@gmail.com>
2026-07-08 15:18:33 +0900

    037

--- back/src/main/java/com/back/global/security/SecurityUser.java
+++ back/src/main/java/com/back/global/security/SecurityUser.java
@@ -11,21 +11,27 @@ import java.util.Map;
 @Getter
 public class SecurityUser extends User implements OAuth2User {
     private final int id;
-    private final String name;
+    private final String nickname;
 
     public SecurityUser(
             int id,
             String username,
-            String name,
+            String nickname,
             Collection<? extends GrantedAuthority> authorities
     ) {
         super(username, "", authorities); // 우리의 시나리오(REST API)에서는 이 객체의 비밀번호 필드를 활용할 일이 없다.
         this.id = id;
-        this.name = name;
+        this.nickname = nickname;
     }
 
     @Override
     public Map<String, Object> getAttributes() {
         return Map.of();
+    }
+
+    // OAuth2User.getName()은 유니크한 값을 리턴해야 하는데, nickname은 유니크하지 않을 수 있으므로 username을 리턴한다.
+    @Override
+    public String getName() {
+        return getUsername();
     }
 }
